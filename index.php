a54cc5fc4f20f5df150c3a3ce6838ffe2450c649
Moultamiss Imane <93978575+moultamissimane@users.noreply.github.com>
2023-09-04 20:11:56 +0100

    add index file

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Rock Paper Scissors Online Game 8e2fa12b</title>
+    <?php require_once "bootstrap.php"; ?>
+</head>
+
+<body>
+    <div class="container">
+        <h1 style="display: flex; justify-content:center;">Welcome to Rock Paper Scissors Online Game</h1>
+        <p>
+            <a href="login.php" style="display: flex; justify-content:center; margin-top: 20px; background-color: black; padding: 20px; color: white; font-size: large; border-radius: 20px;">Log In To Play</a>
+        </p>
+
+    </div>
+</body>
